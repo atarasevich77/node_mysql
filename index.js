e0aade47eafcb40fcf4bf415886d38f18d4d88b7
alex <aleksandrtarasevich77@gmail.com>
2021-02-15 07:18:20 -0700

    Add users actions

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql');
 const bodyParser = require('body-parser');
+const usersRouter = require('./routes/users');
 
 const app = express();
+
+db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '19vilka76',
+    database: 'simplenodeapi'
+});
 
 const server = {
     port: 3000
@@ -10,6 +19,8 @@ const server = {
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use('/users', usersRouter);
+
 app.listen(server.port, () => {
     console.log(`Server started, listening port: ${server.port}`);
 })
