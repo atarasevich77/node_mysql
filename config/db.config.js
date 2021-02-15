f07e34465e5d6dbcf80f96f1ffe65e63aa008395
alex <aleksandrtarasevich77@gmail.com>
2021-02-15 09:41:44 -0700

    Mysql config

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    HOST: process.env.DB_HOST,
+    HOST: process.env.DB_URL,
     USER: process.env.DB_USER,
     PASSWORD: process.env.DB_PASSWORD,
     DB: process.env.DB_NAME
