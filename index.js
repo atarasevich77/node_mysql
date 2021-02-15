de408ca1590b43dc3a523d20232ac1b4e1179fb9
alex <aleksandrtarasevich77@gmail.com>
2021-02-15 08:35:53 -0700

    Server config

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
 const express = require('express');
-const cors = require('cors');
-const mysql = require('mysql');
 const bodyParser = require('body-parser');
-const usersRouter = require('./routes/users');
 
 const app = express();
 
-db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '19vilka76',
-    database: 'simplenodeapi'
+// parse requests of content-type: application/json
+app.use(bodyParser.json());
+// parse requests of content-type: application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+    res.json({ message: 'Welcome to Simple API application.' });
 });
 
-const server = {
-    port: 3000
-};
-
-app.use(cors());
-app.use(bodyParser.json());
-app.use('/users', usersRouter);
-
-app.listen(server.port, () => {
-    console.log(`Server started, listening port: ${server.port}`);
-})
+// set port, listen for requests
+app.listen(3000, () => {
+    console.log('Server started, listening port 3000');
+});
